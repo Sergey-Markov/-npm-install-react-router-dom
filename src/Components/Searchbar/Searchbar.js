6ddb19e7017a3258801c47aa38cf45b76d8851d7
Sergey-Markov <1nativefl@gmail.com>
2021-09-14 18:47:39 +0300

    add pretier

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,47 +1,42 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function Searchbar ({onSubmit}) {
-  
-
-  const [imageName, setImageName] = useState('')
+export default function Searchbar({ onSubmit }) {
+  const [imageName, setImageName] = useState("");
 
   function resset() {
-    setImageName( '' );
+    setImageName("");
   }
 
-  const hadleAddInputValue = e => {
-    setImageName( e.currentTarget.value.toLowerCase().trim());
+  const hadleAddInputValue = (e) => {
+    setImageName(e.currentTarget.value.toLowerCase().trim());
   };
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(imageName);
     resset();
   };
 
-  
-
   return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-          <input
-            onChange={hadleAddInputValue}
-            name="imageName"
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={imageName}
-          />
-        </form>
-      </header>
-    );
-  
+        <input
+          onChange={hadleAddInputValue}
+          name="imageName"
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={imageName}
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
